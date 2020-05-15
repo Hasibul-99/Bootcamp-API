ab6624585fb4a3886da2361d962fea3688dff5cd
hasibul-99 <hasibul_uap@outlook.com>
2020-05-16 00:27:37 +0600

    connecting to the database

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
+const connectDB = require('./config/db');
 //Load env vars
 dotenv.config({path: './config/config.env'});
+
+// Connect to Database
+connectDB();
 
 // Route files
 const bootcamps = require("./routes/bootcamps");
@@ -19,4 +22,12 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handel unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+
+    // Close server & exit process
+    server.close(() => process.exit(1));
+})
